9c369b167dd0bea4425841ab3122e279b7a0cf0b
forlan10 <sdforlan330@gmail.com>
2026-08-11 21:25:59 -0300

    spbs

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://kyptrjpzhtqceqdbewtl.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5cHRyanB6aHRxY2VxZGJld3RsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxNjU2NTksImV4cCI6MjEwMTc0MTY1OX0.dnnrylzko52SXsBtkH4bU3zTRcBdCqH5eiSaYPvEvoI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
